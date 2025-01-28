85be26220e79e5f24efd2c96a10cca2473c80161
Daniel Katz <daniel.katz01@sap.com>
2025-01-28 15:42:21 +0200

    Update test_get_portfolio_value

--- tests/assn4_tests.py
+++ tests/assn4_tests.py
@@ -3,6 +3,7 @@ import requests
 
 BASE_URL = "http://localhost:5001"
 STOCK_IDS = {}
+STOCK_VALUES = {}
 
 # Sample stock data
 stock1 = {"name": "NVIDIA Corporation", "symbol": "NVDA", "purchase price": 134.66, "purchase date": "18-06-2024", "shares": 7}
@@ -46,25 +47,27 @@ def test_get_stocks():
 # Test 4
 # Test GET /stock-value/{ID}
 def test_get_stock_value():
+    global STOCK_STOCK_VALUES
     for symbol in ["NVDA", "AAPL", "GOOG"]:
         response = requests.get(f"{BASE_URL}/stock-value/{STOCK_IDS[symbol]}")
+        STOCK_STOCK_VALUES[symbol] = response.json()["stock_value"]
         assert response.status_code == 200
         assert response.json()["symbol"] == symbol
 
 
-# # Test 5
-# # Test GET /portfolio-value
-# def test_get_portfolio_value():
-#     response = requests.get(f"{BASE_URL}/portfolio-value")
-#     assert response.status_code == 200
-#     pv = response.json()["portfolio value"]
-#     print("Portfolio Value:", pv)
-#     stock1_value = stock1["purchase price"] * stock1["shares"]
-#     stock2_value = stock2["purchase price"] * stock2["shares"]
-#     stock3_value = stock3["purchase price"] * stock3["shares"]
-#     stocks_value = stock1_value + stock2_value + stock3_value
-#     print(f"stocks_value {stocks_value}= Stock1 value: {stock1_value}, Stock2 value: {stock2_value}, Stock3 value: {stock3_value}")
-#     assert pv * 0.97 <= stocks_value <= pv * 1.03
+# Test 5
+# Test GET /portfolio-value
+def test_get_portfolio_value():
+    response = requests.get(f"{BASE_URL}/portfolio-value")
+    assert response.status_code == 200
+
+    pv = response.json()["portfolio value"]
+    print("Portfolio Value:", pv)
+
+    stocks_value = sum(STOCK_VALUES[symbol] for symbol in ["NVDA", "AAPL", "GOOG"])
+    print(f"stocks_value = {stocks_value}")
+
+    assert pv * 0.97 <= stocks_value <= pv * 1.03
 
 
 # Test 6
