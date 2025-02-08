b7c52b5e3e7619382ffbf7aaaa6de86860b56d23
Daniel Katz <danielka1010@gmail.com>
2025-02-08 20:49:01 +0200

    Drop comments and create test response file

--- tests/assn4_tests.py
+++ tests/assn4_tests.py
@@ -9,16 +9,6 @@ STOCK_IDS = {}
 STOCK_VALUES = {}
 STOCK_DATA = []
 
-# # Sample stock data
-# stock1 = {"name": "NVIDIA Corporation", "symbol": "NVDA", "purchase price": 134.66, "purchase date": "18-06-2024", "shares": 7}
-# stock2 = {"name": "Apple Inc.", "symbol": "AAPL", "purchase price": 183.63, "purchase date": "22-02-2024", "shares": 19}
-# stock3 = {"name": "Alphabet Inc.", "symbol": "GOOG", "purchase price": 140.12, "purchase date": "24-10-2024", "shares": 14}
-# stock4 = {"name": "Tesla, Inc.", "symbol": "TSLA", "purchase price": 194.58, "purchase date": "28-11-2022", "shares": 32}
-# stock5 = {"name": "Microsoft Corporation", "symbol": "MSFT", "purchase price": 420.55, "purchase date": "09-02-2024", "shares": 35}
-# stock6 = {"name": "Intel Corporation", "symbol": "INTC", "purchase price": 19.15, "purchase date": "13-01-2025", "shares": 10}
-# stock7 = {"name": "Amazon.com, Inc.", "purchase price": 134.66, "purchase date": "18-06-2024", "shares": 7}
-# stock8 = {"name": "Amazon.com, Inc.", "symbol": "AMZN", "purchase price": 134.66, "purchase date": "Tuesday, June 18, 2024", "shares": 7}
-
 
 # Load stock data from JSON file at the beginning
 @pytest.fixture(scope="session", autouse=True)
@@ -30,8 +20,6 @@ def load_stock_data():
 
 
 # Test 1
-# # Runs once per module and automatically executes before tests
-# @pytest.fixture(scope="module", autouse=True)
 def test_posted_stocks():
     """Posts all initial stocks and stores their IDs globally."""
     global STOCK_IDS
